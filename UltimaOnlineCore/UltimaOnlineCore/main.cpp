b8081d85da529ee88a922308067b0ee46db7311b
Hercules Junior <herculesjr@gmail.com>
2016-04-09 10:19:56 -0300

    project created!

---
+++ UltimaOnlineCore/UltimaOnlineCore/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UltimaOnlineCore
+//
+//  Created by Hercules Junior on 1/16/16.
+//  Copyright © 2016 Hercules Junior. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
